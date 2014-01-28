31604e9a72ffdf7b1b8f2ca23d7d3e3c8500da5e
Simon Holywell <simon@emosaic.co.uk>
2014-01-28 13:06:42 +0000

    Get HHVM extension self building

--- exec.sh
+++ exec.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
+git clean -fd
 cd hhvm-ext
 ./exec.sh
 
--- hhvm-ext/exec.sh
+++ hhvm-ext/exec.sh
@@ -1,2 +1,10 @@
 #! /usr/bin/env bash
+if [[ "$HPHP_HOME" == "" ]]; then
+    echo "HPHP_HOME environment variable must be set!"
+    echo 'export HPHP_HOME=/path/to/hhvm'
+    exit 1
+fi
+$HPHP_HOME/hphp/tools/hphpize/hphpize
+cmake .
+make
 $HPHP_HOME/hphp/hhvm/hhvm -c config.hdf -v "DynamicExtensionPath=`pwd`" test.php
