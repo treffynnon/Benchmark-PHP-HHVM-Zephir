87b7e5aabbb5ec15de00c75509790e0bec2fa0cf
Simon Holywell <simon@emosaic.co.uk>
2014-01-28 17:20:30 +0000

    First attempt at a php extension building mandelbrot

--- php-ext/treffynnon.c
+++ php-ext/treffynnon.c
@@ -2,6 +2,8 @@
 #include "config.h"
 #endif
 
+#include<stdio.h>
+
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/info.h"
@@ -43,6 +45,55 @@ PHP_MINFO_FUNCTION(treffynnon) {
 }
 /* }}} */
 
+int mandelbrot (int arg)
+{
+    int w, h, bit_num = 0;
+    char byte_acc = 0;
+    int i, iter = 50;
+    double x, y, limit = 2.0;
+    double Zr, Zi, Cr, Ci, Tr, Ti;
+    
+    w = h = arg;
+
+    printf("P4\n%d %d\n",w,h);
+
+    for(y=0;y<h;++y) 
+    {
+        for(x=0;x<w;++x)
+        {
+            Zr = Zi = Tr = Ti = 0.0;
+            Cr = (2.0*x/w - 1.5); Ci=(2.0*y/h - 1.0);
+        
+            for (i=0;i<iter && (Tr+Ti <= limit*limit);++i)
+            {
+                Zi = 2.0*Zr*Zi + Ci;
+                Zr = Tr - Ti + Cr;
+                Tr = Zr * Zr;
+                Ti = Zi * Zi;
+            }
+       
+            byte_acc <<= 1; 
+            if(Tr+Ti <= limit*limit) byte_acc |= 0x01;
+                
+            ++bit_num; 
+
+            if(bit_num == 8)
+            {
+                putc(byte_acc,stdout);
+                byte_acc = 0;
+                bit_num = 0;
+            }
+            else if(x == w-1)
+            {
+                byte_acc <<= (8-w%8);
+                putc(byte_acc,stdout);
+                byte_acc = 0;
+                bit_num = 0;
+            }
+        }
+    }   
+}
+
 /* {{{ proto string treffynnon(int arg)
 */
 PHP_FUNCTION(treffynnon) {
@@ -52,6 +103,8 @@ PHP_FUNCTION(treffynnon) {
     if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
         RETURN_NULL();
     }
+
+    mandelbrot(arg);
 
     RETURN_STRING("treffynnon function executed.", 0);
 }
