6002d99f9555a523667535cd17678c0d94eec504
Simon Holywell <simon@emosaic.co.uk>
2014-01-28 14:47:08 +0000

    Make build script use /usr/bin/env

--- c/build.sh
+++ c/build.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-/usr/bin/gcc -pipe -Wall -O3 -fomit-frame-pointer -march=native -std=c99 -mfpmath=sse -msse2 -fopenmp mandelbrot.c -o mandelbrot
+/usr/bin/env gcc -pipe -Wall -O3 -fomit-frame-pointer -march=native -std=c99 -mfpmath=sse -msse2 -fopenmp mandelbrot.c -o mandelbrot
