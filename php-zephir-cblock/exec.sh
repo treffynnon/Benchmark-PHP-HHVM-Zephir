dd65ff473f613611fd99e16cc5b31ac876d0c153
Simon Holywell <simon@emosaic.co.uk>
2014-01-29 12:44:23 +0000

    Pipe output to /dev/null

--- php-zephir-cblock/exec.sh
+++ php-zephir-cblock/exec.sh
@@ -3,4 +3,4 @@ ARG=1000
 if [ "$1" != "" ]; then
     ARG="$1"
 fi
-/usr/bin/env time /usr/bin/env php -n -d "extension=treffynnoncblock/ext/modules/treffynnoncblock.so" -f test.php "$ARG"
+/usr/bin/env time /usr/bin/env php -n -d "extension=treffynnoncblock/ext/modules/treffynnoncblock.so" -f test.php "$ARG" > /dev/null
