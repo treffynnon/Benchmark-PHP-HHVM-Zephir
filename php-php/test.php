1150d1a3c20a76b6f09c36608f51e196a19b08a8
Simon Holywell <treffynnon@php.net>
2014-02-01 21:32:53 +0000

    Update the pure PHP example

--- php-php/test.php
+++ php-php/test.php
@@ -5,25 +5,22 @@
    modified by anon
 */
 
-ob_implicit_flush(1);
-ob_start(NULL, 4096);
-
-
-$h = ($argc == 2) ? ((int)$argv[1]) : 600;
-$w = $h;
-
-printf ("P4\n%d %d\n", $w, $h);
+function write_mandelbrot_to_stream($w, $h, $stream, $bitmap) {
+if($bitmap)
+    fprintf($stream, "P4\n%d %d\n", $w, $h);
 
 $bit_num = 128;
 $byte_acc = 0;
+$iter = 50;
 
 $yfac = 2.0 / $h;
 $xfac = 2.0 / $w;
 
+$ochars = ' .:-;!/>)|&IH%*#';
+$pack_format = 'c*';
+
 for ($y = 0 ; $y < $h ; ++$y)
 {
-   $result = array('c*');
-
    $Ci = $y * $yfac - 1.0;
 
    for ($x = 0 ; $x < $w ; ++$x)
@@ -33,7 +30,7 @@ for ($y = 0 ; $y < $h ; ++$y)
       $Cr = $x * $xfac - 1.5;
 
       do {
-         for ($i = 0 ; $i < 50 ; ++$i)
+         for ($i = 0 ; $i < $iter ; ++$i)
          {
             $Zi = 2.0 * $Zr * $Zi + $Ci;
             $Zr = $Tr - $Ti + $Cr;
@@ -41,20 +38,65 @@ for ($y = 0 ; $y < $h ; ++$y)
             if (($Tr+($Ti = $Zi * $Zi)) > 4.0) break 2;
          }
          $byte_acc += $bit_num;
-      } while (FALSE);
-
-      if ($bit_num === 1) {
-         $result[] = $byte_acc;
-         $bit_num = 128;
-         $byte_acc = 0;
+      } while (false);
+      
+      if($bitmap) {
+          if ($bit_num === 1) {
+             fwrite($stream, pack($pack_format, $byte_acc));
+             $bit_num = 128;
+             $byte_acc = 0;
+          } else {
+             $bit_num >>= 1;
+          }
       } else {
-         $bit_num >>= 1;
+          if($i == $iter) {
+              fwrite($stream, $ochars[0]);
+          } else {
+              fwrite($stream, $ochars[$i & 15]);
+          }
       }
    }
-   if ($bit_num !== 128) {
-      $result[] = $byte_acc;
-      $bit_num = 128;
-      $byte_acc = 0;
+   if($bitmap) {
+       if ($bit_num !== 128) {
+          fwrite($stream, pack($pack_format, $byte_acc));
+          $bit_num = 128;
+          $byte_acc = 0;
+       }
+   } else {
+      fwrite($stream, "\n");
    }
-   echo call_user_func_array('pack', $result);
 }
+return true;
+}
+
+function treffynnon_mandelbrot_to_file($filename, $w, $h, $binary_output) {
+    $file_open_type = 'w';
+    if($binary_output)
+        $file_open_type = 'wb';
+
+    $stream = fopen((string) $filename, $file_open_type);
+    if(false === $stream)
+        return false;
+
+    write_mandelbrot_to_stream((int) $w, (int) $h, $stream, (bool) $binary_output);
+    fclose($stream);
+    return true;
+}
+
+function treffynnon_mandelbrot_to_mem($w, $h, $binary_output) {
+    $file_open_type = 'w+';
+    if($binary_output)
+        $file_open_type = 'w+b';
+
+    $stream = fopen('php://memory', $file_open_type);
+    if(false === $stream)
+        return '';
+
+    write_mandelbrot_to_stream((int) $w, (int) $h, $stream, (bool) $binary_output);
+    rewind($stream);
+    $ret = stream_get_contents($stream);
+    fclose($stream);
+    return $ret;
+}
+
+var_dump(treffynnon_mandelbrot_to_mem((int) $argv[1], (int) $argv[1], false));
