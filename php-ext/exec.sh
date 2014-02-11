344ac78c5922416b2ffb50683344c3a68e97d847
Simon Holywell <simon@emosaic.co.uk>
2014-02-11 13:44:28 +0000

    Enable the OPcache

--- php-ext/exec.sh
+++ php-ext/exec.sh
@@ -6,4 +6,4 @@ fi
 if [ "$2" != "" ]; then
     TIMEFORMAT="--format=$2"
 fi
-/usr/bin/env time "$TIMEFORMAT" /usr/bin/env php -n -d "extension=modules/treffynnon.so" -f test.php "$ARG"
+/usr/bin/env time "$TIMEFORMAT" /usr/bin/env php -n -d "extension=modules/treffynnon.so" -d "zend_extension=opcache.so" -d "opcache.enable_cli=1" -d "opcache.memory_consumption=128" -d "opcache.interned_strings_buffer=8" -d "opcache.revalidate_freq=60" -d "opcache.fast_shutdown=1" -f test.php "$ARG"
--- php-php/exec.sh
+++ php-php/exec.sh
@@ -6,4 +6,4 @@ fi
 if [ "$2" != "" ]; then
     TIMEFORMAT="--format=$2"
 fi
-/usr/bin/env time "$TIMEFORMAT" /usr/bin/php5 -n -f test.php "$ARG"
+/usr/bin/env time "$TIMEFORMAT" /usr/bin/php5 -n -d "zend_extension=opcache.so" -d "opcache.enable_cli=1" -d "opcache.memory_consumption=128" -d "opcache.interned_strings_buffer=8" -d "opcache.revalidate_freq=60" -d "opcache.fast_shutdown=1" -f test.php "$ARG"
