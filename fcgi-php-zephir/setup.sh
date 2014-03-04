430424119fb4a97914b6ae9dfcb283a8bff6679a
Simon Holywell <simon@emosaic.co.uk>
2014-03-04 13:06:01 +0000

    Add Zephir FCGI

---
+++ fcgi-php-zephir/setup.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+echo "Starting FCGI process"
+php-cgi -n -d "zend_extension=opcache.so" -d 'extension=../cli-php-zephir/treffynnon/ext/modules/treffynnon.so' -b /tmp/treffynnon_bench.socket &
+echo "Sleep for a second to allow process to ready itself"
+sleep 1
