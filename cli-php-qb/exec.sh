669442b2364f8036614296f367cb0b075d794fe4
Simon Holywell <treffynnon@php.net>
2014-03-13 22:51:45 +0000

    Add beginnings of CLI test for PHP-QB

---
+++ cli-php-qb/exec.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+ARG=1000
+TIMEFORMAT="-v"
+if [ "$1" != "" ]; then
+    ARG="$1"
+fi
+if [ "$2" != "" ]; then
+    TIMEFORMAT="--format=$2"
+fi
+/usr/bin/env time "$TIMEFORMAT" /usr/bin/env php -n -d "extension=phpqb.so" -d "zend_extension=opcache.so" -d "opcache.enable_cli=1" -f test.php "$ARG"
